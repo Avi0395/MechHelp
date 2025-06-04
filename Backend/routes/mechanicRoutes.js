845dd4fe83d53d0a4d0dfd8f078d2ce49b5b721b
softclones <sahilmalavi96@gmail.com>
2025-06-05 01:11:23 +0530

    prefinal3

--- Backend/routes/mechanicRoutes.js
+++ Backend/routes/mechanicRoutes.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: 'uploads/' }); // or configure storage as needed
-
-
 const mechanicCtrl = require("../controllers/mechanicController");
 const { verifyMechanicToken } = require("../Middleware/authMiddleware");
 
@@ -13,15 +9,10 @@ router.post("/logout", verifyMechanicToken, mechanicCtrl.logoutMechanic);
 
 router.get("/nearby", mechanicCtrl.getNearbyAvailableMechanics);
 router.get("/profiles", mechanicCtrl.getAllMechanics);
-router.get(
-  "/profile",
-  verifyMechanicToken,
-  mechanicCtrl.getMechanicProfile
-);
+router.get("/profile", verifyMechanicToken, mechanicCtrl.getMechanicProfile);
 router.put(
   "/updateprofile",
   verifyMechanicToken,
-  upload.single("profilePic"), // Add this middleware to handle file uploads
   mechanicCtrl.updateMechanicProfile
 );
 router.put(
@@ -29,10 +20,6 @@ router.put(
   verifyMechanicToken,
   mechanicCtrl.updateAvailability
 );
-router.put(
-  "/location",
-  verifyMechanicToken,
-  mechanicCtrl.updateLocation
-);
+router.put("/location", verifyMechanicToken, mechanicCtrl.updateLocation);
 
 module.exports = router;
